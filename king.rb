d7451aa79bb55d5654027a00f7c70d65fed398e9
Jeremy Woertink <jeremywoertink@gmail.com>
2014-08-06 20:09:34 -0700

    all pieces displaying

---
+++ king.rb
@@ -0,0 +1,19 @@
+class King < Piece
+
+  def set_initial_spot
+    row = @color.eql?('white') ? 0 : 7
+    col = @spot
+    @x = (col * @board.block[:height]) + @board.block[:width] / 2
+    @y = row * @board.block[:height] + 1.5
+    @board.layout[row][col] = self
+  end
+
+  def draw
+    @image.draw(@x, @y, 2)
+  end
+
+  def update
+
+  end
+  
+end
--- player.rb
+++ player.rb
@@ -12,6 +12,9 @@ class Player
   def add_pieces
     add_pawns
     add_rooks
+    add_bishops
+    add_knights
+    add_royals
   end
 
   def add_pawns
@@ -23,6 +26,21 @@ class Player
   def add_rooks
     @pieces << Rook.new(@color, 0)
     @pieces << Rook.new(@color, 7)
+  end
+
+  def add_bishops
+    @pieces << Bishop.new(@color, 2)
+    @pieces << Bishop.new(@color, 5)
+  end
+
+  def add_knights
+    @pieces << Knight.new(@color, 1)
+    @pieces << Knight.new(@color, 6)
+  end
+
+  def add_royals
+    @pieces << King.new(@color, 4)
+    @pieces << Queen.new(@color, 3)
   end
 
 end
